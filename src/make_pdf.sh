8e53f616c51464ad35469e5bbc17cc9868c53ba8
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-08-20 12:20:31 -0500

    pdf

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -8,32 +8,39 @@ echo Running $(basename "${BASH_SOURCE}")
 # Work in output directory
 cd ${out_dir}
 
-# Binarize the segmentation and find its center of mass, as a starting point for
-# slice placement in the visualization
-fslmaths seg -bin mask
-com=$(fslstats mask -c)
+# Binarize the mask and find its center of mass, as a starting point for
+# slice placement in the visualization. Voxel coords
+com=$(fslstats fmri_mask${masksize} -C)
 XYZ=(${com// / })
+X=$(printf '%.0f' ${XYZ[0]})
+Y=$(printf '%.0f' ${XYZ[1]})
 
-# Create an axial view of T1 with ROI overlay for a series of axial slices. 
-# Offsets are specified in mm from the computed center of mass of the brain.
-for sl in -040 -030 -020 -010 000 010 020 030 040 050 060; do
+# Show each slice with the mask outline
+nsl=$(fslval fmri_moco_mean.nii.gz dim3)
+zoom=2000
+for Z in $(seq -s ' ' -f '%03.0f' 1 $nsl); do
 
-    Z=$(echo "${XYZ[2]} + ${sl}" | bc -l)
-    echo "    Slice ${sl} at ${XYZ[0]} ${XYZ[1]} ${Z}"
+    echo "    Slice at $X $Y $Z"
 
-    fsleyes render -of slice_${sl}.png \
-        --scene ortho --worldLoc ${XYZ[0]} ${XYZ[1]} ${Z} \
-        --layout horizontal --hideCursor --hideLabels --hidex --hidey \
-        holed_t1 --overlayType volume \
-        holed_seg --overlayType label --lut random_big --outline --outlineWidth 2
+    # Sleep before and after to avoid some kind of race condition or 
+    # disk access delay when testing in docker:
+    # SystemError: wxEntryStart failed, unable to initialize wxWidgets!
+    sleep 1
+    fsleyes render -of slice_${Z}.png \
+        --scene ortho --voxelLoc $X $Y $Z \
+        --xzoom $zoom --yzoom $zoom --zzoom $zoom \
+        --layout horizontal --hideCursor --hidex --hidey \
+        fmri_moco_mean --overlayType volume \
+        fmri_mask${masksize} --overlayType label --lut random_big \
+        --outline --outlineWidth 2
+    sleep 1
 
 done
 
-# Combine into single image using ImageMagick. Pretty hacky way of getting the
-# slices in the right order, but it works
-montage -mode concatenate slice_-0{4,3,2,1}*.png slice_0*.png \
-    -tile 3x4 -quality 100 -background black -gravity center \
-    -border 20 -bordercolor black page1.png
+# Combine into single image using ImageMagick
+montage -mode concatenate slice_*.png \
+    -tile 3x -quality 100 -background black -gravity center \
+    -border 10 -bordercolor black page1.png
 
 # Resize and add text annotations. We choose a large but not ridiculous
 # pixel size for the full page.
@@ -41,13 +48,13 @@ convert \
     -size 2600x3365 xc:white \
     -gravity center \( page1.png -resize 2400x \) -composite \
     -gravity North -pointsize 48 -annotate +0+100 \
-    "Holed segmentation on holed T1" \
+    "Mask on fmri_moco_mean" \
     -gravity SouthEast -pointsize 48 -annotate +100+100 "$(date)" \
     -gravity NorthWest -pointsize 48 -annotate +100+200 "${label_info}" \
     page1.png
 
 # Convert to PDF
-convert page1.png holed_image.pdf
+convert page1.png moco.pdf
 
 # Clean up
-rm page1.png mask.nii.gz slice_*.png
+rm page1.png slice_*.png
